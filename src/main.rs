a2dd5ca9b5702606459ba17fbeb84f1e59050448
koichi <saputrafarel62@gmail.com>
2026-07-24 19:22:38 +0700

    2. testing Tcp

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::net::{TcpListener};
+
 fn main() {
-    println!("Hello, world!");
+    let listen = TcpListener::bind("12.0.0.1:2021")
+        .expect("gagal");
+
+    println!("waiting for..");
+
+    for stream in listen.incoming() {
+        match stream {
+            Ok(_stream) => {
+                println!("yay")
+            },
+            Err(_) => {
+                println!("eror");
+                println!("waiting for cancle");
+                break;
+        }
+        }
+    }
+
 }
