09244c8048bf6b566c659bb86129365aa50d0be3
koichi <saputrafarel62@gmail.com>
2026-07-24 20:42:45 +0700

    3. test log

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,37 @@
 use std::net::{TcpListener};
+use std::io::{Read, Write};
 
 fn main() {
-    let listen = TcpListener::bind("12.0.0.1:2021")
+//TODO: local host, ganti ke port komputer masing masing yah
+// 
+    let listen = TcpListener::bind("0.0.0.0:8080")
         .expect("gagal");
 
     println!("waiting for..");
 
     for stream in listen.incoming() {
-        match stream {
-            Ok(_stream) => {
-                println!("yay")
+       let mut stream =  match stream {
+            Ok(stream) => {
+                println!("yay");
+                stream
             },
             Err(_) => {
                 println!("eror");
                 println!("waiting for cancle");
                 break;
-        }
-        }
+            }
+        };
+        
+        let mut now = [0; 1024];
+
+        let test = stream.read(&mut now).unwrap();
+        
+
+        let pesan = String::from_utf8_lossy(&now[..test]);
+        println!("something : ");
+        println!("{}", pesan);
+
+        stream.write_all(b"hallo").unwrap();
     }
 
 }
