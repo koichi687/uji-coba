3f28e4c61fb6afedf0d0f18722a86f6f8de470be
koichi <saputrafarel62@gmail.com>
2026-07-25 19:46:08 +0700

    4. benerin fn, masih perlu fix buat htmlnya

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,27 @@
-use std::net::{TcpListener};
-use std::io::{Read, Write};
+use std::{io, net::{TcpListener, TcpStream}};
+use std::io::{Write};
+
+fn handle(stream: io::Result<TcpStream>) {
+    match stream {
+        Ok(mut stream) => {
+            println!("hello");
+            let body_test = "hello guy";
+            let respon =  format!("HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}", body_test.len(), body_test);
+            stream.write_all(respon.as_bytes()).expect("err");
+            stream.flush().expect("gagal flush");
+            println!("response terkirim");
+        },
+        Err(_) => {
+            println!("eror, repeat again")
+        }
+    }
+}   
 
 fn main() {
-//TODO: local host, ganti ke port komputer masing masing yah
-// 
-    let listen = TcpListener::bind("0.0.0.0:8080")
-        .expect("gagal");
-
-    println!("waiting for..");
+    let listen = TcpListener::bind("0.0.0.0:221").expect("eror");
 
     for stream in listen.incoming() {
-       let mut stream =  match stream {
-            Ok(stream) => {
-                println!("yay");
-                stream
-            },
-            Err(_) => {
-                println!("eror");
-                println!("waiting for cancle");
-                break;
-            }
-        };
-        
-        let mut now = [0; 1024];
-
-        let test = stream.read(&mut now).unwrap();
-        
-
-        let pesan = String::from_utf8_lossy(&now[..test]);
-        println!("something : ");
-        println!("{}", pesan);
-
-        stream.write_all(b"hallo").unwrap();
+        handle(stream);
     }
-
 }
+
